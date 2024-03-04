2bb81d14043029cc5b509631c8901bb5482a0530
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2024-03-04 19:38:41 +0700

    add login page

--- resources/views/pariwisata.blade.php
+++ resources/views/pariwisata.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href={{url("assets/css/custom.css")  }}>
     <link rel="stylesheet" href={{url("fonts/fonts.css")  }}>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <title>main</title>
+    <title>Pariwisata</title>
 
 </head>
 
