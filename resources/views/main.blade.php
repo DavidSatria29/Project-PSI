07a6de9a8ecced145843b00cc83804ac06592eb6
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2024-03-04 10:20:02 +0700

    1

---
+++ resources/views/main.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href={{url("assets/css/bootstrap.min.css")  }}>
+    <title>main</title>
+    <style>
+      .beta{
+        color: white;
+        background-color: midnightblue
+      }
+    </style>
+</head>
+
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar scroll</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    
+    <div class="section bg-primary">
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <h1>Kampung Wisata Keramik Dinoyo</h1>
+          </div>
+          <div class="row">
+            <h3>Kampung Wisata Keramik Dinoyo merupakan destinasi wisata yang berada di Kota Malang.</h3>
+          </div>
+          <div class="row">
+            <div class="col">
+              <button type="button" class="btn btn-warning">Warning</button>
+            </div>
+            <div class="col">
+              <button type="button" class="btn btn-outline-light">Light</button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <img src="..." class="rounded mx-auto d-block" alt="...">
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="row">
+        <h1>Sejarah</h1>
+        <div class="col-4">
+          <img src="..." class="rounded mx-auto d-block" alt="...">
+        </div>
+        <div class="col-6">
+          <p>Lorem ipsum</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="row">
+        <h1>Lokasi dan Jam Operasional</h1>
+        <div class="col">
+          <h1>SENIN - MINGGU
+            08.00 - 15.00</h1>
+        </div>
+        <div class="col">
+          <img src="..." class="rounded mx-auto d-block" alt="...">
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="row">
+        <h1>Fasilitas</h1>
+        <div class="col">
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+        </div>
+        <div class="col">
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+        </div>
+        <div class="col">
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+          <div class="row">
+            <img src="..." class="rounded mx-auto d-block" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- border card --}}
+    <div class="section beta">
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <img src="..." class="card-img-top" alt="...">
+          </div>
+          <div class="row">
+            <h5>lorem ipsum</h5>
+          </div>
+          <div class="row"></div>
+        </div>
+        <div class="col">
+          <div class="row">
+            <h5>Company</h5>
+          </div>
+          <div class="row">
+            <p>Sejarah</p>
+          </div>
+          <div class="row">
+            <p>Lokasi dan Operasional</p>
+          </div>
+          <div class="row">
+            <p>Tiket masuk</p>
+          </div>
+          <div class="row">
+            <p>Fasilitas</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="row">
+            <h5>Resource</h5>
+          </div>
+          <div class="row">
+            <p>Produk Keramik</p>
+          </div>
+          <div class="row">
+            <p>Produk Wisata</p>
+          </div>
+          <div class="row">
+            <p>Workshop Keramik</p>
+          </div>
+          <div class="row">
+            <p>Pemesanan Produk</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="row">
+            <h5>Newslatter</h5>
+          </div>
+          <div class="row"></div>
+          <div class="row"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section beta">
+      <div class="row">
+        <div class="col">
+          <p>About us</p>
+        </div>
+        <div class="col">
+          <p>Contact</p>
+        </div>
+        <div class="col">
+          <p>Privacy Policy</p>
+        </div>
+        <div class="col">
+          <p>Sitemap</p>
+        </div>
+        <div class="col">
+          <p>Terms of Use</p>
+        </div>
+        <div class="col"></div>
+        <div class="col">
+          <p>© 2024, All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
+    <script src={{ url("assets/js/bootstrap.bundle.min.js") }}></script>
+</body>
+</html>
