0ad2f5e60f5076f1c0cff867f275386084cd2442
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2024-04-01 08:11:56 +0700

    Lorem Ipsum Done

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -48,162 +48,157 @@
 			</div>
 		</div>
 		<!-- End Lokasi dan Jam Operasional Section -->
-			<!-- Start Fasilitas -->
-				<div class="popular-product">
-					<div class="container">
-						<div class="row justify-content-between">
-							<h1 class="section-title text-center" style="color: #12498C"><b>FASILITAS</b></h1>
+    <!-- Start Fasilitas -->
+    <div class="popular-product">
+        <div class="container">
+            <div class="row justify-content-between">
+                <h1 class="section-title text-center" style="color: #12498C"><b>FASILITAS</b></h1>
 
-							<div class="row">
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Nordic Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-			
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Kruzo Aero Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-			
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Ergonomic Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-	
-							</div>
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Wisata</h3>
+                                <p>Nikmati petualangan alam yang memukau di lokasi wisata terbaik kami.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
 
-							<div class="row">
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Nordic Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-			
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Kruzo Aero Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-			
-								<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-									<div class="product-item-sm d-flex">
-										<div class="thumbnail">
-											<img src="images/dummy.png" alt="Image" class="img-fluid">
-										</div>
-										<div class="pt-3">
-											<h3>Ergonomic Chair</h3>
-											<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
-											<p><a href="#">Read More</a></p>
-										</div>
-									</div>
-								</div>
-	
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				<!-- End Fasilitas -->
-		
-		<!-- Start Review Section -->
-		<div class="blog-section">
-			<div class="container">
-				<div class="row mb-5">
-					<h1 class="section-title text-center" style="color: #12498C"><b>REVIEW</b></h1>
-					<div class="col-md-6">
-					</div>
-					<div class="col-md-6 text-start text-md-end">
-						<a href="#" class="more">View All Posts</a>
-					</div>
-				</div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Cengkrama Perajin</h3>
+                                <p>Temukan keindahan dalam kerajinan tangan dari perajin lokal kami.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
 
-				<div class="row">
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Sovenir Keramik</h3>
+                                <p>Bawa pulang kenangan abadi dengan koleksi sovenir keramik kami.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
 
-					<div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
-						<div class="post-entry">
-							<a href="#" class="post-thumbnail">
-								<div class="card" style="width: 18rem;">
-									<img src="images/card1.png" class="card-img-top p-3" alt="...">
-									<div class="card-body">
-									  <h4 class="card-title text-center">Lorem ipsum</h4>
-									  <h5 class="card-text text-center">-Lisa-</h5>
-									  <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-									</div>
-								  </div>
-							</a>
-						</div>
-					</div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Buat Keramik Sendiri</h3>
+                                <p>Ekspresikan kreativitas Anda dengan membuat keramik sendiri di bengkel kami.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
 
-					<div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
-						<div class="post-entry">
-							<a href="#" class="post-thumbnail">
-								<div class="card" style="width: 18rem;">
-									<img src="images/card3.png" class="card-img-top p-3" alt="...">
-									<div class="card-body">
-									  <h4 class="card-title text-center">Lorem ipsum</h4>
-									  <h5 class="card-text text-center">-Lisa-</h5>
-									  <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-									</div>
-								  </div>
-							</a>
-						</div>
-					</div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Layanan Premium</h3>
+                                <p>Nikmati pengalaman istimewa dengan layanan premium kami.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
 
-					<div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
-						<div class="post-entry">
-							<a href="#" class="post-thumbnail">
-								<div class="card" style="width: 18rem;">
-									<img src="images/card2.png" class="card-img-top p-3" alt="...">
-									<div class="card-body">
-									  <h4 class="card-title text-center">Lorem ipsum</h4>
-									  <h5 class="card-text text-center">-Lisa-</h5>
-									  <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-									</div>
-								  </div>
-							</a>
-						</div>
-					</div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="product-item-sm d-flex">
+                            <div class="thumbnail">
+                                <img src="images/dummy.png" alt="Image" class="img-fluid">
+                            </div>
+                            <div class="pt-3">
+                                <h3>Pelayanan Ramah</h3>
+                                <p>Dapatkan pelayanan ramah dan profesional dari staf kami yang siap membantu.</p>
+                                <p><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Fasilitas -->
 
-				</div>
-			</div>
-		</div>
-		<!-- End Review Section -->	
+    <!-- Start Review Section -->
+    <div class="blog-section">
+        <div class="container">
+            <div class="row mb-5">
+                <h1 class="section-title text-center" style="color: #12498C"><b>REVIEW</b></h1>
+                <div class="col-md-6">
+                </div>
+                <div class="col-md-6 text-start text-md-end">
+                    <a href="#" class="more">View All Posts</a>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail">
+                            <div class="card" style="width: 18rem;">
+                                <img src="images/card1.png" class="card-img-top p-3" alt="...">
+                                <div class="card-body">
+                                    <h4 class="card-title text-center">Vas Bunga</h4>
+                                    <h5 class="card-text text-center">-Bagas-</h5>
+                                    <p class="card-text text-justify">Elegan dan berkelas, vas bunga ini menjadi sentuhan indah untuk rumah Anda. Desainnya yang menawan dan kualitasnya yang prima membuatnya menjadi pilihan yang sempurna untuk menghiasi ruangan Anda.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail">
+                            <div class="card" style="width: 18rem;">
+                                <img src="images/card3.png" class="card-img-top p-3" alt="...">
+                                <div class="card-body">
+                                    <h4 class="card-title text-center">Topeng Hias</h4>
+                                    <h5 class="card-text text-center">-David-</h5>
+                                    <p class="card-text text-justify">Topeng hias yang menawan ini adalah perpaduan sempurna antara seni dan budaya. Dibuat dengan detail yang cermat, setiap unsur dalam topeng ini menghadirkan keindahan dan keanggunan yang memikat.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail">
+                            <div class="card" style="width: 18rem;">
+                                <img src="images/card2.png" class="card-img-top p-3" alt="...">
+                                <div class="card-body">
+                                    <h4 class="card-title text-center">Piring Keramik</h4>
+                                    <h5 class="card-text text-center">-Amanda-</h5>
+                                    <p class="card-text text-justify">Piring keramik ini tidak hanya fungsional, tetapi juga merupakan karya seni yang mempesona. Dengan motif yang unik dan warna yang cerah, setiap hidangan Anda akan terlihat lebih istimewa di atas piring ini.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Review Section -->
 @endsection
--- resources/views/customer/tourist.blade.php
+++ resources/views/customer/tourist.blade.php
@@ -1,44 +1,45 @@
 @extends('layouts.CustomerMaster')
 @section('tourist', 'active')
 @section('content')
-{{-- Paket Wisata --}}
-    <div class="section beta default_h">
-      <div class="container py-4">
-        <div class="row">
-          <h1 class="text-center" style="color: #12498C"><b>PAKET WISATA</b></h1>
-          <div class="col d-flex" style="justify-content: center">
-            <div class="card" style="width: 18rem;">
-              <img src="images/paket1.png" class="card-img-top p-3" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-center">Wisata 1</h4>
-                <h5 class="card-text text-center" style="color: red">Rp.200,000</h5>
-                <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-              </div>
-            </div>
+{{-- Start Paket Wisata Section--}}
+<div class="section beta default_h">
+  <div class="container py-4">
+    <div class="row">
+      <h1 class="text-center" style="color: #12498C"><b>PAKET WISATA</b></h1>
+      <div class="col d-flex" style="justify-content: center">
+        <div class="card" style="width: 18rem;">
+          <img src="images/paket1.png" class="card-img-top p-3" alt="...">
+          <div class="card-body">
+            <h4 class="card-title text-center">Ukir Keramik</h4>
+            <h5 class="card-text text-center" style="color: red">Rp.200,000</h5>
+            <p class="card-text text-justify">Nikmati pengalaman mengukir keramik yang unik dan personal dalam paket wisata ini. Dibimbing oleh para ahli, Anda akan menemukan keindahan dalam setiap goresan yang Anda buat. Biaya: Rp.200,000.</p>
           </div>
-          <div class="col d-flex" style="justify-content: center">
-            <div class="card" style="width: 18rem;">
-              <img src="images/paket2.png" class="card-img-top p-3" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-center">Wisata 2</h4>
-                <h5 class="card-text text-center" style="color: red">Rp.300,000</h5>
-                <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div class="col d-flex" style="justify-content: center">
+        <div class="card" style="width: 18rem;">
+          <img src="images/paket2.png" class="card-img-top p-3" alt="...">
+          <div class="card-body">
+            <h4 class="card-title text-center">Kerajinan Keramik</h4>
+            <h5 class="card-text text-center" style="color: red">Rp.300,000</h5>
+            <p class="card-text text-justify">Jelajahi seni kerajinan keramik yang menakjubkan dalam paket wisata ini. Dari proses pembuatan hingga finishing, Anda akan belajar teknik-teknik yang memukau dari para perajin terampil. Biaya: Rp.300,000.</p>
           </div>
-          <div class="col d-flex" style="justify-content: center">
-            <div class="card" style="width: 18rem;">
-              <img src="images/paket3.png" class="card-img-top p-3" alt="...">
-              <div class="card-body">
-                <h4 class="card-title text-center">Wisata 3</h4>
-                <h5 class="card-text text-center" style="color: red">Rp.400,000</h5>
-                <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis id justo pretium, id bibendum ipsum lobortis. Donec vel hendrerit ante. Integer id justo ut est porta sollicitudin.</p>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div class="col d-flex" style="justify-content: center">
+        <div class="card" style="width: 18rem;">
+          <img src="images/paket3.png" class="card-img-top p-3" alt="...">
+          <div class="card-body">
+            <h4 class="card-title text-center">Ukir dan Bikin Keramik</h4>
+            <h5 class="card-text text-center" style="color: red">Rp.400,000</h5>
+            <p class="card-text text-justify">Dapatkan pengalaman lengkap dengan mengukir dan membuat keramik sendiri dalam paket wisata ini. Temukan kebahagiaan dalam menciptakan karya seni Anda sendiri, mulai dari konsep hingga produk jadi. Biaya: Rp.400,000.</p>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
+{{-- End Paket Wisata Section --}}
 
     {{-- Start Workshop Keramik Section --}}
     <div class="section beta default_h">
